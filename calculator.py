2e995178feb479ca64f33507ee86c7ec58146c7c
visweswar <visweswar299@gmail.com>
2021-04-28 21:22:22 -0700

    update

--- calculator.py
+++ calculator.py
@@ -1,9 +1,5 @@
 from datetime import datetime
 
-today = datetime.today()
-
-print("Current date =", today)
-
 
 def add(a, b):
     return a+b
@@ -26,20 +22,51 @@ def modulo(a, b):
 
 
 def power(a, b):
-    return a**b
+    return a ** b
 
 
-def square(a):
-    return a*a
-
-
-def factorial(a):
-    if a == 1 or a == 0:
-        return 1
+while True:
+    f = open("myfile.txt", "a")
+    print("\n-------------------------------------------------------------------------------------------\n")
+    print(datetime.now())
+    print(datetime.now(), file=f)
+    input1 = input("Enter a number or type 'STOP' to turn off: ")
+    if input1 == "STOP" or input1 == "stop" or input1 == "Stop":
+        print("Turning off.")
+        print("Turning off.", file=f)
+        break
+    elif not input1.isdigit():
+        print("This is not a valid number.")
+        continue
     else:
-        fact = 1
-        for i in range(1, a+1):
-            fact *= i
-        return fact
-
-
+        number1 = int(input1.strip())
+        print("Enter a number or type 'STOP' to turn off:" + str(number1), file=f)
+    operator = input("Enter an operation (+, -, *, /, %, or **): ")
+    operator = operator.strip()
+    print("Enter an operation (+, -, *, /, or %):" + str(operator), file=f)
+    input2 = input("Enter another number: ")
+    if not input2.isdigit():
+        print("This is not a valid number.")
+        continue
+    else:
+        number2 = int(input2.strip())
+        print("Enter a number or type 'STOP' to turn off:" + str(number2), file=f)
+    if operator == '+':
+        ans = add(number1, number2)
+    elif operator == '-':
+        ans = subtract(number1, number2)
+    elif operator == '*':
+        ans = multiply(number1, number2)
+    elif operator == '/':
+        ans = divide(number1, number2)
+    elif operator == '%':
+        ans = modulo(number1, number2)
+    elif operator == '**':
+        ans = power(number1, number2)
+    else:
+        print("You did not enter a valid operation.")
+        print("You did not enter a valid operation.", file=f)
+        continue
+    print(number1, operator, number2, "=", ans)
+    print(str(number1), str(operator), str(number2), "=", str(ans), file=f)
+    f.close()
