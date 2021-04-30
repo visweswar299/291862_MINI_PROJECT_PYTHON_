1f6e5e25345a555615a0d1ef07439605dfd1885d
visweswar <visweswar299@gmail.com>
2021-04-29 21:36:43 -0700

    update

--- calculator.py
+++ calculator.py
@@ -1,35 +1,47 @@
+# import datetime to use date and time
 from datetime import datetime
 
 
+# add function to find addition of two numbers
 def add(a, b):
     return a+b
 
 
+# subtract function to find subtraction of two numbers
 def subtract(a, b):
     return a-b
 
 
+# multiplication function to find (n times of m number)
 def multiply(a, b):
     return a*b
 
 
+# division function to find quotient of the numbers
 def divide(a, b):
     return a/b
 
 
+# modulus function to find remainder of the numbers
 def modulo(a, b):
     return a % b
 
 
+# power function to find power of numbers
 def power(a, b):
     return a ** b
 
 
 while True:
+    # myfile.txt is to store the calculated values.
     f = open("myfile.txt", "a")
     print("\n-------------------------------------------------------------------------------------------\n")
     print(datetime.now())
     print(datetime.now(), file=f)
+    '''
+    taking first number to calculate other wise type stop to turn off
+    if entered value is not digit then it shows invalid number
+    '''
     input1 = input("Enter a number or type 'STOP' to turn off: ")
     if input1 == "STOP" or input1 == "stop" or input1 == "Stop":
         print("Turning off.")
@@ -41,6 +53,7 @@ while True:
     else:
         number1 = int(input1.strip())
         print("Enter a number or type 'STOP' to turn off:" + str(number1), file=f)
+    # operator - enter what operation do you want
     operator = input("Enter an operation (+, -, *, /, %, or **): ")
     operator = operator.strip()
     print("Enter an operation (+, -, *, /, or %):" + str(operator), file=f)
@@ -69,4 +82,5 @@ while True:
         continue
     print(number1, operator, number2, "=", ans)
     print(str(number1), str(operator), str(number2), "=", str(ans), file=f)
+    # file closing
     f.close()
